fe815cec9eaee700a16ff91ae3ce1c3df6458a44
XJAHED <103598554+XJAHED@users.noreply.github.com>
2023-09-17 23:43:10 +0600

    Add files via upload

---
+++ database/database.php
@@ -0,0 +1,13 @@
+
+
+<?php
+
+$databasehostname = 'localhost';
+$databasename = 'root';
+$databasepassword = '';
+$dataname = 'yammi_food';
+
+$connect = mysqli_connect($databasehostname, $databasename, $databasepassword, $dataname);
+
+
+?>
